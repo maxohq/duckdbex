f73dea9a6b763ff865145be72de7ad27c8db873c
Roman Heinrich <roman.heinrich@gmail.com>
2023-11-27 23:15:41 +0100

    Chore: add checksum.exs, so we dont need to publish on hex.pm

---
+++ checksum.exs
@@ -0,0 +1,6 @@
+%{
+  "duckdbex-nif-2.15-aarch64-apple-darwin-0.2.7.tar.gz" => "sha256:53948687b267a83307f643ab2d2e339537fe79fbc85a22aa3e5f9d495f4fb198",
+  "duckdbex-nif-2.15-x86_64-apple-darwin-0.2.7.tar.gz" => "sha256:087838c939d83ddb6d06f9cd4b6e9422a3fd67c7c43642111c90021eed9e6b5e",
+  "duckdbex-nif-2.17-aarch64-apple-darwin-0.2.7.tar.gz" => "sha256:6942fa611c4793cd9abd406f72050899a1502cac78ec6359ca2f3aae35254a72",
+  "duckdbex-nif-2.17-x86_64-apple-darwin-0.2.7.tar.gz" => "sha256:79d99e5e3b15b1e2c2178cdcd621d2b0112a4f31589b00ff096d3a9e6c815a15",
+}
